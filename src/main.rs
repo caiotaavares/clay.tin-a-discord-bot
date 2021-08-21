32b9a813295ffdef73036dba5b5b39f6f0c334fe
caiotaavares <caio_s.tavares2@hotmail.com>
2021-08-21 18:15:24 -0300

    ping,pong feature

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,26 @@ extern crate serenity;
 
 use serenity::prelude::*;
 use serenity::model::gateway::Ready;
+use serenity::model::channel::Message;
 
-const TOKEN : &str = "ODc4NzM3OTE3OTkwODAxNDQ4.YSFidQ.KEKLuB4ZOOicGjElL5faYRlpLAQ";
+const TOKEN : &str = "ODc4NzM3OTE3OTkwODAxNDQ4.YSFidQ.mdxEA9S2aO1IebnAumdKKALlyFY";
 
 struct Handler;
 
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+
+    fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "?ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong?") {
+                println!("Error giving message: {:?}", why);
+            }
+        }
+    }
+
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is ready", ready.user.name);
+    }
+}
 
 fn main() {
 
