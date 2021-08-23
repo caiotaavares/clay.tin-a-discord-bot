632ca6084151ab6b10238a4567ca0b48ee6a7c68
caiotaavares <caio_s.tavares2@hotmail.com>
2021-08-23 14:26:23 -0300

    external token file

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,17 @@ use serenity::prelude::*;
 use serenity::model::gateway::Ready;
 use serenity::model::channel::Message;
 
-const TOKEN : &str = "ODc4NzM3OTE3OTkwODAxNDQ4.YSFidQ.mdxEA9S2aO1IebnAumdKKALlyFY";
+// use std::fs::read_to_string;
+
+use std::io::prelude::*;
+use std::fs::File;
 
 struct Handler;
 
 impl EventHandler for Handler {
 
     fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "?ping" {
+        if msg.content == "-c ping" {
             if let Err(why) = msg.channel_id.say(&ctx.http, "Pong?") {
                 println!("Error giving message: {:?}", why);
             }
@@ -25,9 +28,14 @@ impl EventHandler for Handler {
 
 fn main() {
 
-    let mut client = Client::new(&TOKEN, Handler)
-                        .expect("Error creating client");
-        
+    let mut file = File::open(".token").unwrap();
+    let mut token = String::new();
+    file.read_to_string(&mut token)
+                            .expect("Token file not found");
+
+    let mut client = Client::new(&token, Handler)
+                            .expect("Error creating client");
+    
     if let Err(msg) = client.start() {
         println!("Error: {:?}", msg);
     }
